fd403ca0e5d7f1f2619b1c0ff27ae3f3e506b126
ItsJustDice <ckdebruin@gmail.com>
2023-02-16 06:03:40 -0800

    Delacreme

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Hello, printf
- * Description:Using printf to print text
+ * Description: Using printf to print text
  * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammer, but the outcome is a piece of art, \n");
 	return (0);
 }
