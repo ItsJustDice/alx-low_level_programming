8db3f7932432b30737d2365878ace85095413e47
ItsJustDice <ckdebruin@gmail.com>
2023-02-16 05:44:17 -0800

    Blonde

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: Print 'Programming is like building a multilingual puzzle"
+ * Description: Print "Programming is like building a multilingual puzzle"
  * Return: 0
  */
-
 int main(void)
 {
-	puts("\"Programming is like bulding a multilingual puzzle");
-		return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
