e2c5be1ef6d423db080c0dff60e9a89f9ac085d1
ItsJustDice <ckdebruin@gmail.com>
2023-02-17 10:05:28 -0800

    River

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print single digits from 0 - 9
+ * Return: 0
+ */
+int main(void)
+{
+	int nums;
+
+	for (nums = 0; num <= 9; nums++)
+		putchar((nums % 10) + '0');
+	putchar('\n');
+
+	return (0);
+}
