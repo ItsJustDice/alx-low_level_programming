385dc47c3634776d82c504a547f5af0deab45b64
ItsJustDice <ckdebruin@gmail.com>
2023-02-17 11:54:18 -0800

    SolSol

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description; Combinations of single-digit numbers.
+ * Description - Combinations of single-digit numbers.
  * Return: 0
  */
 int main(void)
 {
-	int il
+	int i;
 
 		for (i = 48; i < 58; i++)
 		{
