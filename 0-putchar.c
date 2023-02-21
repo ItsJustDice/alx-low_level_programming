9174309e35abb23dcf5a878167d6283dd42d99f0
ItsJustDice <ckdebruin@gmail.com>
2023-02-21 02:49:49 -0800

    0-putchar.c

---
+++ 0-putchar.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <main.h>
+/**
+ * main - Entry point
+ * Description - putchar writes character c to stdout
+ * @c - Character to print
+ * Return: )
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (write(1, % c, 1));
+}
