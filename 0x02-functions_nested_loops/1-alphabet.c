0d14b8645ba41f4c09122692737230e2bb4c01a5
ItsJustDice <ckdebruin@gmail.com>
2023-02-21 04:51:28 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ * Description - Playing the alphabet game
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
