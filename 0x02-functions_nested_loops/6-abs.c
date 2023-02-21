f1bbc62bc6d8c0e7d37d5cec96ab1057d24a4b39
ItsJustDice <ckdebruin@gmail.com>
2023-02-21 10:00:19 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - Entry point
+ * Description: a function that computes the absolute value of an integer
+ * @r: Integer
+ * Return: 0
+ */
+int _abs(int r)
+{
+	if (r < 0)
+	{
+		return (-0);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
