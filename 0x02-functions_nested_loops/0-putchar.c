5a31b96ab1d6418a57b734712f22464f1789987f
ItsJustDice <ckdebruin@gmail.com>
2023-02-21 03:48:24 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r')
+	char c[] = "_putchar\n";
+	int i = 0;
 
-		return (write(1, % c, 1));
+	while (c[i] != '\0')
+	{
+		_putchar(c[i]);
+		i++;
+	}
+
+	return (0);
 }
