9200c01a31cdb0fb911d9f5a4913288e6e931f6c
ItsJustDice <ckdebruin@gmail.com>
2023-02-22 09:14:34 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_times_table - Entry point
- * Description: Times table
+ * Description - times table
  * @n: Integer
  * Return: int
  */
@@ -11,40 +11,37 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; <= n; i++)
+		for (j = 0; j <= n; j++)
 		{
-			for (j = 0; j <= n; j++)
+			ans = i * j;
+			if (ans > 99)
 			{
-				ans = i * j;
-				if (ans > 99)
-				{
-					_putchar(',');
-					_putchar(32);
-					_putchar((ans / 100) + '0');
-					_putchar(((ans / 10) % 10) + '0');
-					_putchar((ans % 10) + '0');
-				}
-				else if (ans > 9)
-				{
-					_putchar(',');
-					_putchar(32);
-					_putchar(32);
-					_putchar(((ans / 10) % 10) + '0');
-					_putchar((ans % 10) + '0');
-				}
-				else
-				{
-					if (j != 0)
-					{
-						_putchar(',');
-						_putchar(32);
-						_putchar(32);
-						_putchar(32);
-					}
-					_putchar(ans + '0');
-				}
+				_putchar(',');
+				_putchar(32);
+				_putchar((ans / 100) + '0');
+				_putchar(((ans / 10) % 10) + '0');
+				_putchar((ans % 10) + '0');
 			}
-			_putchar('\n');
+			else if (ans > 9)
+			{
+				_putchar(',');
+				_putchar(32);
+				_putchar(32);
+				_putchar(((ans / 10) % 10) + '0');
+				_putchar((ans % 10) + '0');
+			}
+			else
+			{
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(ans + '0');
+			}
 		}
+		_putchar('\n');
 	}
 }
