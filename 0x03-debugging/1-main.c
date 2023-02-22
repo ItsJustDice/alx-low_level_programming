89af594bffc606fd94e0cf931eb4603ce8b05420
ItsJustDice <ckdebruin@gmail.com>
2023-02-22 10:41:13 -0800

    1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - An infinite loop
+ * Description - endless loop
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+	/**
+	 * while (i < 10)
+	 * {
+	 *       putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avaoided! \\o/\n");
+
+	return (0);
+}
