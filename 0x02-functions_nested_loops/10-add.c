c075c4368dd45e4d0b34d30f6581a2f314dabc2c
ItsJustDice <ckdebruin@gmail.com>
2023-02-22 02:51:20 -0800

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * int_add - Entry point
+ * add - Entry point
  * Description: a function that adds two integers and returns the result
  * @a: Integer
  * @b: Integer
- * Return: Int
+ * Return: int
  */
-int int_add(int a, int b)
+int add(int a, int b)
 {
 	int ans = a + b;
 
