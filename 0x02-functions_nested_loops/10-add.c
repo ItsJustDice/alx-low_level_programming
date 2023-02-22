29237b791cf0ffab5726598a7af8190d2004fcc6
ItsJustDice <ckdebruin@gmail.com>
2023-02-22 02:43:00 -0800

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,38 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * int add - Entry point
+ * int_add - Entry point
  * Description: a function that adds two integers and returns the result
- * Return: result 0
+ * @a: Integer
+ * @b: Integer
+ * Return: Int
  */
-int add(int a, int b)
+int int_add(int a, int b)
 {
-	int result = a + b;
+	int ans = a + b;
 
-	if (result < 0)
-	{
-		_ putchar('-');
-		result = -result;
-	}
-	if (result > 9)
-	{
-		int power = 1;
-		while (power * 10 <= result);
-		{
-			power *= 10;
-		}
-		 while (power > 0)
-		 {
-			 int digit = result / power;
-			 _putchar(digit + '0');
-			 result -= digit * power;
-			 power /= 10;
-		 }
-	}
-	else
-	{
-		putchar(result + '0');
-	}
-	_putchar('\n');
-	return result;
+	return (ans);
 }
