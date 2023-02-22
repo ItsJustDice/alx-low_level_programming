7729db06c66bf212403cc4cfa1e25762f6aee88f
ItsJustDice <ckdebruin@gmail.com>
2023-02-22 04:54:27 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,7 +11,7 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; <= n; i++)
+		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
@@ -41,10 +41,10 @@ void print_times_table(int n)
 						_putchar(32);
 						_putchar(32);
 					}
-					_putchar(ans + '0');
+					_purchar(ans + '0');
 				}
 			}
-			_putchar('\n')
+			_putchar('\n');
 		}
 	}
 }
