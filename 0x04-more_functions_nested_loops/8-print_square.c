22764ccf92ed8893cdcdda26472958033f41ae5b
ItsJustDice <ckdebruin@gmail.com>
2023-02-23 04:30:05 -0800

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - Entry Point
+ * Description - a function that prints a square
+ * @size: Integer
+ * Return: void
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
