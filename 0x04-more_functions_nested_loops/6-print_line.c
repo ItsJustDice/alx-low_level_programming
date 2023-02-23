165011f5dfa5e81d8476bc371013891f9a9f89d9
ItsJustDice <ckdebruin@gmail.com>
2023-02-23 04:02:03 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - Entry point
+ * Description - a function that draws a straight line in the terminal
+ * @n: Integer
+ * Return: void
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
