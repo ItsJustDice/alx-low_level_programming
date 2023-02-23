98010946149be1c36203a326895022e6b13753f2
ItsJustDice <ckdebruin@gmail.com>
2023-02-23 03:26:17 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Entry point
+ * Description - a function that multiplies two integers
+ * @a: Integer
+ * @b: Integer
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
