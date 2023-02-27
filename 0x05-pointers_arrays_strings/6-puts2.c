9b79e48eb57bc608522c939c95d2ffb216c22561
ItsJustDice <ckdebruin@gmail.com>
2023-02-27 12:32:18 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - Entry point
+ * Description: Print characters
+ * @str: Character
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+		if (j % 2 == 0)
+		{
+			_putchar(str[j]);
+		}
+		j++;
+	}
+	_putchar('\n');
+}
