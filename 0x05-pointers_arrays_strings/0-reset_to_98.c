09b6e33841dc4182d0573ff46ce8637f67fa29d3
ItsJustDice <ckdebruin@gmail.com>
2023-02-27 02:02:04 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98  - Entry point
+ * Decription:a function that resets to 98
+ * @n: Integer
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	int *n;
+
+	*n = 98;
+	_putchar(*n);
+}
