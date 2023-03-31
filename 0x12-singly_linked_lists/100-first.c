cc0566e023054c46ca4c1d8ae4fd376d36779dfe
ItsJustDice <ckdebruin@gmail.com>
2023-03-31 14:36:56 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * Description: function that runs before main
  * Return: nothing
  */
-void __attribute__ ((constructor)) hush();
+void __attribute__ ((constructor)) hush()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
