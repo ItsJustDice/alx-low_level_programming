73a4680b381f5f2e5f7b9a20e122a1eedc6da4f4
ItsJustDice <ckdebruin@gmail.com>
2023-03-31 14:34:40 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * premain - function that runs before main
+ * premain - Entry point
+ * Description: function that runs before main
  * Return: nothing
  */
-void __attribute__ ((constructor)) premain()
+void __attribute__ ((constructor)) hush();
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
