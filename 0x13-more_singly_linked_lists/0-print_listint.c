4ef6ea277534a1e148a73b464ed93d26716dc1b2
ItsJustDice <ckdebruin@gmail.com>
2023-04-03 04:59:35 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_listint - Entry point
+ * Description: prints all the elements of a listint_t list
+ * @h: Integer
+ * Return: return to number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t matrix = 0;
+	/* hatched to keep track of the number of nodes */
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		matrix++;
+		h = h->next;
+	}
+	/*"h" is next node, "next" is member of current node and moves loop forward*/
+	return (matrix);
+}
