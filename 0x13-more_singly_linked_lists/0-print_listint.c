01fb20a5bf49f955b7e46e817313436ab41d58f0
ItsJustDice <ckdebruin@gmail.com>
2023-04-03 09:51:08 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,7 +10,7 @@ size_t print_listint(const listint_t *h)
 	size_t matrix = 0;
 	/* hatched to keep track of the number of nodes */
 
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		matrix++;
