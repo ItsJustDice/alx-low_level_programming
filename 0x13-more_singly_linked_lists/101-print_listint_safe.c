5c6e821c387ff241ef5c62d45bcc6d955737b3b7
ItsJustDice <ckdebruin@gmail.com>
2023-04-04 20:02:20 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,21 +9,28 @@ size_t print_listint_safe(const listint_t *head)
 {
 	size_t hop = 0;
 	/* sets count variable to 0 */
+	const listint_t *temp;
+
+	if (!head || !head->next)
+	{
+		return (0);
+	}
 	while (head != NULL)
 	{
-		hop++;
-		/* accrual count for each node */
+		/* sets present to head & shifts head to following node */
+		temp = head;
+		head = head->next;
+		/* prints current node */
 		printf("[%p] %d\n", (void *)head, head->n);
 		/* prints node's adress and value */
-		if (head <= head->next)
-			/* checks to see if there's a cycle in the list */
+		hop++;
+		/* accrual count for each node */
+		if (temp <= temp->next)
 		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			printf("-> [%p] %d\n", (void *)temp->next, temp->next->n);
 			exit(98);
 			/* exit with 98 if there's a cycle */
 		}
-		head = head->next;
-		/* move head to next node */
 	}
 	return (hop);
 }
