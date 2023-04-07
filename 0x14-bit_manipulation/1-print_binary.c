a67dff0efa222a30d31108123d2844c9af12415c
ItsJustDice <ckdebruin@gmail.com>
2023-04-07 04:58:39 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,19 +9,15 @@ void print_binary(unsigned long int n)
 {
 	int j;
 	/* go through each bit in the integer in iteration */
+	unsigned long int dip;
+	/* extract number's i-th bit, use bitwise AND, 1UL to create bit mask */
 	for (j = 63; j >= 0; j--)
 	{
-		/* to extract the number's i-th bit, use Bitwise AND */
-		unsigned long int dip = 1UL << j;
-
+		dip = 1UL << j;
 		/* Depending on wheter the bit is set or not, print "1" or "0" */
 		if ((n & dip) != 0)
-		{
 			_putchar ('1');
-		}
 		else
-		{
 			_putchar ('0');
-		}
 	}
 }
