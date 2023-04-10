8118ffd3aca1baa5cee439e29eb6ae2f28c26ab3
ItsJustDice <ckdebruin@gmail.com>
2023-04-10 13:01:44 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,34 +10,29 @@ int create_file(const char *filename, char *text_content)
 {
 	int wri, fd, matrix = 0;
 
-	/* give -1 back if filename is 0 */
+	/* give -1 back if filename is NULL */
 	if (!filename)
-	{
 		return (-1);
-	}
-	/* open specified file, write & truncate if exists, if not then create */
-	/* put 0600 as file permission (rw- --- ---) */
+	/*
+	 * open specified file, write & truncate if exists, if not then create
+	 * put 0600 as file permission (rw- --- ---)
+	 */
 	fd = open(filename, O_CREAT, O_WRONLY, O_TRUNC, 0600);
 	if (fd == -1)
-	{
-		/* opening file failed,if fd was set-to -1 */
-		/* return -1 in the case to show that function failed */
-		return (-1);
-	}
-	/* determine length if text_content isn't empty */
+		/*
+		 * opening file failed,if fd was set-to -1
+		 * return -1 in the case to show that function failed
+		 * determine length if text_content isn't empty
+		 */
 	if (text_content)
 	{
-		/* for string's lenght, make a loop through it */
-		/* the loop keeps going until it reached a NULL  character('0') */
-		/* which denoytes the strings conclusion */
+		/* tallies numbers of charcters in text_context */
 		while (text_content[matrix])
 			matrix++;
 		/* creates file with text_content */
 		wri = write(fd, text_content, matrix);
 		if (wri == -1)
 		{
-			/* close the file descriptor and return to -1 */
-			/* if write() fails, indicating that function failed */
 			close(fd);
 			return (-1);
 		}
