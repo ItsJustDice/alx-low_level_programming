00d0128b5990b2eb4e606ddc91e6fe1f895a0c51
Itsjustdice <ckdebruin@gmail.com>
2023-05-04 21:42:49 -0700

    Test files, Header files added

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+
+/*
+ * Description: Header file for prototypes in 0x14-bit_manipulation directory
+ */
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+int get_endianness(void);
