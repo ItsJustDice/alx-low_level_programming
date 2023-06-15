499f337186f6898c94f47c065649933d15f71d2f
Itsjustdice <ckdebruin@gmail.com>
2023-06-15 11:22:26 -0700

    103-keygen.c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -10,23 +10,23 @@
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	char password[7], *matrix;
+	char password[7], *codex;
 	int len = strlen(argv[1]), j, tmp;
 
 	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
 	tmp = (len ^ 59) & 63;
-	password[0] = matrix[tmp];
+	password[0] = codex[tmp];
 
 	tmp = 0;
 	for (j = 0; j < len; j++)
 		tmp += argv[1][j];
-	password[1] = matrix[(tmp ^ 79) & 63];
+	password[1] = codex[(tmp ^ 79) & 63];
 
 	tmp = 1;
 	for (j = 0; j < len; j++)
 		tmp *= argv[1][j];
-	password[2] = matrix[(tmp ^ 85) & 63];
+	password[2] = codex[(tmp ^ 85) & 63];
 
 	tmp = 0;
 	for (j = 0; j < len; j++)
@@ -35,16 +35,16 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 			tmp = argv[1][j];
 	}
 	srand(tmp ^ 14);
-	password[3] = matrix[rand() & 63];
+	password[3] = codex[rand() & 63];
 
 	tmp = 0;
 	for (j = 0; j < len; j++)
 		tmp += (argv[1][j] * argv[1][j]);
-	password[4] = matrix[(tmp ^ 239) & 63];
+	password[4] = codex[(tmp ^ 239) & 63];
 
 	for (j = 0; j < argv[1][0]; j++)
 		tmp = rand();
-	password[5] = matrix[(tmp ^ 229) & 63];
+	password[5] = codex[(tmp ^ 229) & 63];
 
 	password[6] = '\0';
 	printf("%s", password);
