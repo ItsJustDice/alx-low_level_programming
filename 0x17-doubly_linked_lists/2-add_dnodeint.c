b237144150f3f8ef40df24d4044bdf8f2718aa4d
Itsjustdice <ckdebruin@gmail.com>
2023-06-15 07:00:17 -0700

    2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -7,8 +7,7 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new; /* state new node n*/
-	dlistint_t *h;
+	dlistint_t *new; /* state new node */
 
 	new = malloc(sizeof(dlistint_t)); /* assign memory for new node */
 	if (new == NULL)
@@ -16,19 +15,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new->n = n; /* place value of new node to input value */
 	new->prev = NULL; /* set former pointer of new node to NULL */
-	h = *head; /* set following point of new node to current head */
+	new->next = *head; /* set following point of new node to current head */
 
-	if (head != NULL)
-	{
-		while (h->prev != NULL)
-			h = h->prev; /* Move pointer to head of doubly linked list */
-	}
-
-	new->prev = h; /* Set prev pointer of new node to current head or NULL */
-
-	if (h != NULL)
-		h->prev = new; /* If the list is not empty */
-	/* set the previous pointer of the current head to the new node */
+	if (*head != NULL)
+		(*head)->prev = new;
 
 	*head = new; /* set new node as new head of the list */
 
